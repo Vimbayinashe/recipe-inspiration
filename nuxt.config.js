6a76f117ed2ecf2fa3eba7b11ffdb256e22407f8
Vimbayinashe <vimbayinashemandaza@gmail.com>
2021-05-26 18:11:35 +0200

    Add Bootstrap-Vue (#1)

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,6 +32,7 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    '@bootstrap-vue/nuxt',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
