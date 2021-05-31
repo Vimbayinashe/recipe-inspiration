440c7b3f5c81faf0686e847b6875eaad1349745b
Vimbayinashe <vimbayinashemandaza@gmail.com>
2021-05-31 12:34:30 +0200

    Add random recipe to home page (#5)

--- pages/index.vue
+++ pages/index.vue
@@ -22,9 +22,108 @@
           </b-col>
         </b-row>
 
-      </b-container>
-      
-     
+        <b-row align-h="center" class="mt-5">
+          <b-col cols="auto">
+            <h3>Would you like to try {{ random.strMeal }}?</h3>
+          </b-col>
+        </b-row>
+        <b-row align-h="center">
+          <b-col cols="auto">
+            <!-- restrict image size -->
+            <b-img :alt="'a picture of ' + random.strMeal" fluid :src="random.strMealThumb" />
+          </b-col>
+        </b-row>
+        <b-row class="mt-4">
+          <b-col cols="6" offset-md="3">
+            <h4>Ingredients</h4>
+            <ol>
+              <li v-if="random.strMeasure1 && random.strIngredient1">
+                {{ random.strMeasure1 }} {{ random.strIngredient1}}
+              </li>
+              <li v-if="random.strMeasure2 && random.strIngredient2">
+                {{ random.strMeasure2 }} {{ random.strIngredient2}}
+              </li>
+              <li v-if="random.strMeasure3 && random.strIngredient3">
+                {{ random.strMeasure3 }} {{ random.strIngredient3}}
+              </li>
+              <li v-if="random.strMeasure4 && random.strIngredient4">
+                {{ random.strMeasure4 }} {{ random.strIngredient4}}
+              </li>
+              <li v-if="random.strMeasure5 && random.strIngredient5">
+                {{ random.strMeasure5 }} {{ random.strIngredient5}}
+              </li>
+              <li v-if="random.strMeasure6 && random.strIngredient6">
+                {{ random.strMeasure6 }} {{ random.strIngredient6}}
+              </li>
+              <li v-if="random.strMeasure7 && random.strIngredient7">
+                {{ random.strMeasure7 }} {{ random.strIngredient7}}
+              </li>
+              <li v-if="random.strMeasure8 && random.strIngredient8">
+                {{ random.strMeasure8 }} {{ random.strIngredient8}}
+              </li>
+              <li v-if="random.strMeasure9 && random.strIngredient9">
+                {{ random.strMeasure9 }} {{ random.strIngredient9}}
+              </li>
+              <li v-if="random.strMeasure10 && random.strIngredient10">
+                {{ random.strMeasure10 }} {{ random.strIngredient10}}
+              </li>
+              <li v-if="random.strMeasure11 && random.strIngredient11">
+                {{ random.strMeasure11 }} {{ random.strIngredient11}}
+              </li>
+              <li v-if="random.strMeasure12 && random.strIngredient12">
+                {{ random.strMeasure12 }} {{ random.strIngredient12}}
+              </li>
+              <li v-if="random.strMeasure13 && random.strIngredient13">
+                {{ random.strMeasure13 }} {{ random.strIngredient13}}
+              </li>
+              <li v-if="random.strMeasure14 && random.strIngredient14">
+                {{ random.strMeasure14 }} {{ random.strIngredient14}}
+              </li>
+              <li v-if="random.strMeasure15 && random.strIngredient15">
+                {{ random.strMeasure15 }} {{ random.strIngredient15}}
+              </li>
+              <li v-if="random.strMeasure16 && random.strIngredient16">
+                {{ random.strMeasure16 }} {{ random.strIngredient16}}
+              </li>
+              <li v-if="random.strMeasure17 && random.strIngredient17">
+                {{ random.strMeasure17 }} {{ random.strIngredient17}}
+              </li>
+              <li v-if="random.strMeasure18 && random.strIngredient18">
+                {{ random.strMeasure18 }} {{ random.strIngredient18}}
+              </li>
+              <li v-if="random.strMeasure19 && random.strIngredient19">
+                {{ random.strMeasure19 }} {{ random.strIngredient19}}
+              </li>
+              <li v-if="random.strMeasure20 && random.strIngredient20">
+                {{ random.strMeasure20 }} {{ random.strIngredient20}}
+              </li>
+            </ol>
+          </b-col>
+        </b-row>
+        <b-row class="mt-2">
+          <b-col>
+            <h4>Instructions</h4>
+            <p>{{ random.strInstructions }}</p>
+          </b-col>
+        </b-row>
+
+        <b-row class="mt-2">
+          <b-col>
+            <h4>Watch recipe:</h4>
+            <b-embed 
+              allowfullscreen 
+              :src="random.strYoutube" 
+              type="iframe"
+            />
+          </b-col>
+        </b-row>
+        <b-row class="mt-3">
+          <b-col>
+            <span class="h5">Source: </span>{{ random.strSource }}
+          </b-col>
+        </b-row>
+
+      </b-container>     
     </div>
   </div>
 </template>
@@ -38,11 +137,13 @@ export default {
     const categories = await $axios.$get('/categories.php')
     const random = await $axios.$get('/random.php')
 
+    console.log(random.meals[0]);
+
     return {
       allIngredients: allIngredients.meals,
       areas: areas.meals,
       categories: categories.categories,
-      random,
+      random: random.meals[0],
     }
   },
   components: {
